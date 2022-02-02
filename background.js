e7a37da52aca285e3add5de49efda0c2e81fed10
igorlogius <67047467+igorlogius@users.noreply.github.com>
2022-02-02 13:03:26 +0100

    chore: add icon feat: disable tabs on a per window basis

--- background.js
+++ background.js
@@ -1,27 +1,38 @@
 
-let active = true;
-
-browser.browserAction.setBadgeText({text: (active?'on':'off') });
+browser.browserAction.setBadgeText({text: 'on' });
 browser.browserAction.setBadgeBackgroundColor({color: 'green'});
 
-// move tabs into windows when active
+const inactive_windows = new Set();
+
+// move tabs into new windows
 browser.tabs.onCreated.addListener( async (tab) => {
-	if (active && tab.index !== 0){ // ignore first tab
-	  const srcwin = await browser.windows.get(tab.windowId);
-	  browser.windows.create({
-		  tabId: tab.id, // moves a tab of the specified ID into the new window.
-		  incognito: tab.incognito,
-		  focused: tab.active,
-		  state: srcwin.state,
-	  });
-	}
+    if(!inactive_windows.has(tab.windowId)){
+        if (tab.index !== 0){ // ignore first tab
+            const srcwin = await browser.windows.get(tab.windowId);
+            browser.windows.create({
+                tabId: tab.id, // moves a tab into the new window
+                incognito: tab.incognito,
+                focused: tab.active,
+                state: srcwin.state,
+            });
+        }
+    }
 });
 
 browser.browserAction.onClicked.addListener((tab) => {
-    active = !active; // toggle active state
-	browser.browserAction.setBadgeText({text: (active?'on':'off') });
-    browser.browserAction.setBadgeBackgroundColor({color: (active?'green':'red')});
+    if(inactive_windows.has(tab.windowId)) {
+        inactive_windows.delete(tab.windowId);
+    	browser.browserAction.setBadgeText({tabId: tab.id, text: 'on' });
+        browser.browserAction.setBadgeBackgroundColor({tabId: tab.id, color: 'green'});
+    }else{
+        inactive_windows.add(tab.windowId);
+    	browser.browserAction.setBadgeText({tabId: tab.id, text: 'off' });
+        browser.browserAction.setBadgeBackgroundColor({tabId: tab.id, color: 'red'});
+    }
 });
 
-
-
+browser.windows.onRemoved.addListener((windowId) => {
+    if(inactive_windows.has(windowId)) {
+        inactive_windows.delete(tab.windowId); // cleanup
+    }
+});
